29e30d12ca9f3e4c22fd861d1366d35bc9325905
NguyenDuyThang <thangduy2501@gmail.com>
2025-11-27 21:18:15 +0700

    hoan thanh bai 1

---
+++ Session 2/Tạo cơ sở dữ liệu và bảng quản lý sách/Tạo cơ sở dữ liệu và bảng quản lý sách.sql
@@ -0,0 +1,10 @@
+create database LibraryDB;
+create schema library;
+set search_path to library;
+create table Books(
+  book_id serial primary key,
+    title varchar(100) not null,
+    author varchar(50) not null,
+    published_year int,
+    price float
+);
